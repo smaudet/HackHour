86e35dac392b98f061254c800b1929b867577ae1
Sebastian Audet <Sebastian.Audet@crown.com>
2014-10-31 12:28:27 -0400

    Adding resources to repository

---
+++ CodeMash/lib/codeMashLibrary.js
@@ -0,0 +1,13 @@
+console.log("hello world from codemash lib")
+define(function(require){
+  // var $ = require('jquery')
+  // var s = require('simple')
+  // var d3 = require('d3')
+  
+  // s.sayHello('Person');
+  // var sObj = new s.MyClass(2);
+  //sObj.someProp=5;
+  // var res = sObj.someFunc(4);
+  // $('p').text("Hello From RequireJS: "+res);
+  
+});
